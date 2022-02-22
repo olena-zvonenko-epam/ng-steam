5c0b7deb67f0d3524bf222d11e2d35f9f1d2324f
Elena Zvonenko <elena.zvonenko15@gmail.com>
2022-02-22 21:31:04 +0200

    Environment edit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment: Environment = {
   production: true,
   apiKey: 'AIzaSyCPl8hlSqAsqvWCvxH283jxmjPhBfkeKD0',
   FbDbUrl: 'https://ng-steam-default-rtdb.europe-west1.firebasedatabase.app',
-  FbAuthUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword'
+  FbAuthUrl: 'https://identitytoolkit.googleapis.com/v1/accounts'
 };
