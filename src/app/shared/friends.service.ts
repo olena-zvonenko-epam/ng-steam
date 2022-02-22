37ea045d18dff13c16452beba99720e35d2efaf9
RuslanPostnikov <rp02081992@gmail.com>
2022-02-22 16:01:20 +0200

    Friends and games service update

--- src/app/shared/friends.service.ts
+++ src/app/shared/friends.service.ts
@@ -2,55 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { PersonModel } from "./person.model";
 import { map } from 'rxjs';
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
-// export class FriendsService {
-//
-//   url = 'http://localhost:3000';
-//
-//   constructor(private http: HttpClient) { }
-//
-//   getPeople() {
-//     return this.http.get<GameModel[]>(`${this.url}/people`);
-//   }
-//
-//   getFriends() {
-//     return this.http.get<GameModel[]>(`${this.url}/friends`);
-//   }
-//
-//   addFriend(person: PersonModel) {
-//     this.getFriends()
-//       .subscribe(friends => {
-//         if(friends.find(friend => friend.id === person.id)) {
-//           return alert(`${person.name} is already your friend` );
-//         } else {
-//           alert(`${person.name} was added to your friends` );
-//           return this.http.post(`${this.url}/friends`, person)
-//             .subscribe(person => console.log(person))
-//         }
-//       });
-//   }
-//
-//   removeFriend(person: PersonModel) {
-//     this.getFriends()
-//       .subscribe(friends => {
-//         if(friends.find(friend => friend.id === person.id)) {
-//           alert(`${person.name} was removed from your friends` );
-//           return this.http.delete(`${this.url}/friends/${person.id}`)
-//             .subscribe(person => console.log(person))
-//         } else {
-//           return alert(`${person.name} is not your friend` );
-//         }
-//       });
-//   }
-//
-// }
 
 export class FriendsService {
 
-  url = 'https://ng-steam-default-rtdb.europe-west1.firebasedatabase.app';
+  url = environment.FbDbUrl;
 
   constructor(private http: HttpClient) { }
 
@@ -96,7 +56,7 @@ export class FriendsService {
       });
   }
 
-  removeFriend(person: any) {
+  removeFriend(person: PersonModel) {
     this.getFriends()
       .subscribe(friends => {
         if (friends.find(friend => friend.id === person.id)) {
