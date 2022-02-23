888e21339df7ae40eec55867be04af0ffc327182
Elena Zvonenko <elena.zvonenko15@gmail.com>
2022-02-23 15:10:57 +0200

    Logout button

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private auth: AuthService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  logout(): void {
+    this.auth.logout();
+    this.router.navigate(['/'])
   }
-
 }
