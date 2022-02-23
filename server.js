a253eec1958610dba98d8275ea892dca432be0c7
Elena Zvonenko <elena.zvonenko15@gmail.com>
2022-02-23 15:11:48 +0200

    Fix server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/ng-steam'));
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/ng-steam/index.html'));
 });
 
